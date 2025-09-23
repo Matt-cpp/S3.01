9e23bfc0f1f419510e42bca7feed93cd2cd852cb
Boltz-Louis <boltzlouis44@gmail.com>
2025-09-23 16:38:52 +0200

    backend pa

--- Presentation/page d'accueil.php
+++ Presentation/page d'accueil.php
@@ -1,4 +1,12 @@
 <?php
-$db = getDatabase()
-
+$db = getDatabase();
+$query = "SELECT nom,prenom,cours,date,motif,statut FROM absence " ;
+$statement = $db->getConnection()->prepare($query);
+$statement->execute();
+$absences = $statement->fetchAll(PDO::FETCH_ASSOC);
+//             error_log("Erreur lors de l'exécution de la requête SELECT: " . $e->getMessage());
+//             throw new Exception("Erreur lors de la récupération des données");
+//         }
+//     }
+// }
 ?>
