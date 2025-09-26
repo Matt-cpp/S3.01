225448ba0e449b5898705c8e27e91ed8cd9588d4
Boltz-Louis <boltzlouis44@gmail.com>
2025-09-26 09:24:13 +0200

    Update tableauDeBord.php

--- Presenter/tableauDeBord.php
+++ Presenter/tableauDeBord.php
@@ -2,25 +2,22 @@
 class backendTableauDeBord {
     private $page;
     private $alldata;
-        public
-    function __construct() {
-            require_once __DIR__ . '/../Model/database.php';
-            $this->page = 0;
-            $db = getDatabase();
-            $allData = $db->select('SELECT * FROM absences');
-        }
-    public function getData($page) {
+    private $db;
+
+    public function __construct() {
+        $this->page = 0;
         require_once __DIR__ . '/../Model/database.php';
-        $db = getDatabase();
+        $this->db = Database::getInstance();
+        $this->alldata = $this->db->select('SELECT * FROM absences');
+    }
+    public function getData($page) {
         $offset = $page * 5;
-        $query = "SELECT student_identifier,course_slot_id,status,justified FROM absences order by updated_at desc limit 5 offset $offset";
-        return $db->select($query);
+        $query = "SELECT id,student_identifier,course_slot_id,status,justified FROM absences ORDER BY updated_at DESC, id ASC LIMIT 5 OFFSET :offset";
+        return $this->db->select($query, ['offset' => $offset]);
     }
 
     public function getTotalPages() {
-        require_once __DIR__ . '/../Model/database.php';
-        $db = getDatabase();
-        $result = $db->select("SELECT COUNT(*) as count FROM absences");
+        $result = $this->db->select("SELECT COUNT(*) as count FROM absences");
         return ceil($result[0]['count'] / 5);
     }
     public function setPage($page) {
@@ -28,29 +25,75 @@ class backendTableauDeBord {
             $this->page = $page;
         }
     }
+    public function nextPage() {
+        if ($this->page < $this->getTotalPages() - 1) {
+            $this->page++;
+        }
+    }
+    public function previousPage() {
+        if ($this->page > 0) {
+            $this->page--;
+        }
+    }
+    public function getCurrentPage() {
+        return $this->page;
+    }
+
+    public function getNextPage() {
+        return min($this->page + 1, $this->getTotalPages() - 1);
+    }
+
+    public function getPreviousPage() {
+        return max($this->page - 1, 0);
+    }
+    public function todayAbs() {
+        $query = "SELECT COUNT(*) as count FROM absences WHERE DATE(updated_at) = CURRENT_DATE";
+        $res = $this->db->select($query);
+        return $res[0]['count'];
+    }
+    public function unjustifiedAbs() {
+        $query = "SELECT COUNT(*) as count FROM absences WHERE justified = false";
+        $res = $this->db->select($query);
+        return $res[0]['count'];
+    }
+    public function thisMonthAbs() {
+        $query = "SELECT COUNT(*) as count FROM absences WHERE EXTRACT(YEAR FROM created_at) = EXTRACT(YEAR FROM CURRENT_DATE) AND EXTRACT(MONTH FROM created_at) = EXTRACT(MONTH FROM CURRENT_DATE)";
+        $res = $this->db->select($query);
+        return $res[0]['count'];
+    }
+}
+
+$test = new backendTableauDeBord();
+echo "Nombre d'absences aujourd'hui : ";
+echo $test->todayAbs();
+echo "<br>";
+echo "Nombre d'absences non justifiées : ";
+echo $test->unjustifiedAbs();
+echo "<br>";
+echo "Nombre d'absences ce mois-ci : ";
+echo $test->thisMonthAbs();
+echo "<br>";
+if (isset($_GET['page'])) {
+    $page = intval($_GET['page']);
+    $test->setPage($page);
 }
 ?>
-
-<! a incorporer dans les fonctions php >
-<?php  
-
-$pages = isset($_GET['page']) ? (int)$_GET['page'] : 0;  
-$nextPage = $pages + 1;
-$prevPage = $pages > 0 ? $pages - 1 : 0;
-?>
-
-<a href="?page=<?php echo $nextPage; ?>">
-    <button type="button">next</button>
-</a>
-<a href="?page=<?php echo $prevPage; ?>">
+<a href="?page=<?php echo $test->getPreviousPage(); ?>">
     <button type="button">previous</button>
 </a>
+<a href="?page=<?php echo $test->getNextPage(); ?>">
+    <button type="button">next</button>
+</a>
+
 <br>
 
-<! les fonctions qui fonctionnent >
+
 <?php
 require_once __DIR__ . '/../Model/database.php';
-$test=new backendTableauDeBord();
-$f=$test->getData(0);
+
+$f=$test->getData($test->getCurrentPage());
 echo json_encode($f);
-$nbpages = $test->getTotalPages();
+$nbpages = $test->getTotalPages();?>
+<br>
+Current Page: <?php echo $test->getCurrentPage() + 1; ?> /
+<?php echo $nbpages; ?>
