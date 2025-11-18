9dfb2ff297a597fa54614954865c7885154963af
Boltz-Louis <boltzlouis44@gmail.com>
2025-11-18 16:33:56 +0100

    on prepare le tableau (requete encore a finir)

--- Presenter/LesEvaluations.php
+++ Presenter/LesEvaluations.php
@@ -19,7 +19,21 @@ class pageEvalProf
         $result = $this->db->select($query);
         return $result[0]['identifier'];
     }
-    
+    public function lesEvaluations()
+    {
+        // query pas finie
+        $query = "SELECT resources.label, course_slots.course_date,
+        course_slots.start_time,count(//absences) as nb_absences,
+        count(//abs just) as nb_justifications
+        
+        FROM course_slots Left join resources on course_slots.subject_identifier=resources.code
+        
+        WHERE course_slots.teacher_id=" . $this->userId . " 
+        and course_slots.is_evaluation
+        group by course_slots.subject_identifier";
+    }
+
+
 
     
 
