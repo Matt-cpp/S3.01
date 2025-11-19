73b67a5dde1e5dd3807e66899531d907f5627601
Boltz-Louis <boltzlouis44@gmail.com>
2025-11-19 09:51:24 +0100

    requete sans erreurs (peut etre pas encore fonctionel ceci dit)

--- Presenter/LesEvaluations.php
+++ Presenter/LesEvaluations.php
@@ -1,40 +1,42 @@
-<meta charset="UTF-8">
 <?php
 class pageEvalProf
 {
     private $db;
     private $userId;
-    //constructeur
+    
     public function __construct(int $id)
     {
         require_once __DIR__ . '/../Model/database.php';
         $this->db = Database::getInstance();
-        $this->userId = this->linkTeacherUser($id);
-        
+        $this->userId = $this->linkTeacherUser($id);
     }
+    
     public function linkTeacherUser(int $id)
     {
-        $query = "SELECT teacher.id FROM teachers left JOIN users ON teachers.email=users.email
-        WHERE users.id=" . $id . ""; 
+
+        $query = "SELECT teachers.id as id
+        FROM users LEFT JOIN teachers ON teachers.email = users.email
+        WHERE users.id = " . $id; 
+
         $result = $this->db->select($query);
-        return $result[0]['identifier'];
+        return $result[0]['id']; // Changé ici
     }
+    
     public function lesEvaluations()
     {
-        // query pas finie
-        $query = "SELECT resources.label, course_slots.course_date,
-        course_slots.start_time,count(//absences) as nb_absences,
-        count(//abs just) as nb_justifications
-        
-        FROM course_slots Left join resources on course_slots.subject_identifier=resources.code
-        
-        WHERE course_slots.teacher_id=" . $this->userId . " 
-        and course_slots.is_evaluation
-        group by course_slots.subject_identifier";
+        // CORRECTION: syntaxe SQL complète et correcte
+        $query = "SELECT resources.label, course_slots.course_date,course_slots.start_time,COUNT(absences.student_identifier) as nb_absencesCOUNT,COUNT (CASE WHEN absences.justified = True THEN 1 END) as nb_justifications
+        FROM course_slots LEFT JOIN resources ON course_slots.subject_identifier = resources.code
+         LEFT JOIN absences ON course_slots.id = absences.course_slot_id
+         WHERE course_slots.teacher_id = " . $this->userId . " AND course_slots.is_evaluation = True
+         GROUP BY course_slots.id, resources.label, course_slots.course_date, course_slots.start_time
+         ORDER BY course_slots.course_date DESC, course_slots.start_time DESC;";
+
+        $result = $this->db->select($query);
+        return $result;
     }
-
-
-
-    
-
 }
+
+$a = new pageEvalProf(13);
+$b = $a->lesEvaluations();
+var_dump($b);
