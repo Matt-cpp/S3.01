4c53a0f6973f5dfdbee7effe3f4ef224b12b63a4
Boltz-Louis <boltzlouis44@gmail.com>
2025-11-24 09:46:18 +0100

    jsp

---
+++ View/templates/teacher_evals.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Tableau des Evaluations</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php echo __DIR__ . '/../assets/css/teacher_evals.css'; ?>">
+</head>
+<body>
+    <h1>Mes Evaluations</h1>
+    <p>Prise de vue des evaluations passées.</p>
+</body>
+</html>
