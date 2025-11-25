3c5a500b46a6cc339fb17ae8d2526ae0cfafac65
Boltz-Louis <boltzlouis44@gmail.com>
2025-11-25 11:24:51 +0100

    back end fonctionel

--- Presenter/LesEvaluations.php
+++ Presenter/LesEvaluations.php
@@ -23,30 +23,30 @@ class pageEvalProf
         return $result[0]['id'];
     }
     
-    public function lesEvaluations()
+    public function lesEvaluations($arg)
     {
         // CORRECTION: syntaxe SQL complète et correcte
-        $query = "SELECT resources.label, course_slots.course_date,course_slots.start_time,COUNT(absences.student_identifier) as nb_absencesCOUNT,COUNT (CASE WHEN absences.justified = True THEN 1 END) as nb_justifications
+        $query = "SELECT resources.label, course_slots.course_date,course_slots.start_time, COUNT(*) as nbabs ,COUNT (CASE WHEN absences.justified = True THEN 1 END) as nb_justifications
         FROM course_slots LEFT JOIN resources ON course_slots.subject_identifier = resources.code
          LEFT JOIN absences ON course_slots.id = absences.course_slot_id
          WHERE course_slots.teacher_id = " . $this->userId . " AND course_slots.is_evaluation = True
          GROUP BY course_slots.id, resources.label, course_slots.course_date, course_slots.start_time
-         ORDER BY course_slots.course_date DESC, course_slots.start_time DESC;";
-
-        $result = $this->db->select($query);
-        return $result;
-    }
-    public function exemple()
-    {
-        $query= "SELECT * from absences;";
+         ORDER BY  "  .$arg." DESC, course_slots.start_time DESC;";
+//course_slots.course_date
         $result = $this->db->select($query);
         return $result;
     }
     
 }
 $test = new pageEvalProf(13);
-$evals = $test->exemple();
-var_dump($evals);
-
+/*evals = $test->exemple();
+foreach ($evals as $eval) {
+    echo "ID Teacher: " . $eval['id'] . " - Justifications: " . $eval['cpt'] . "\n";
+}
+*/
+$evals = $test->lesEvaluations("course_slots.course_date");
+foreach ($evals as $eval) {
+    echo "Matière: " . $eval['label'] . " - Date: " . $eval['course_date'] . " - Heures: " . $eval['start_time'] . " - Absences: " . $eval['nbabs'] . " - Justifications: " . $eval['nb_justifications'] . "\n";
+}
 
 ?>
