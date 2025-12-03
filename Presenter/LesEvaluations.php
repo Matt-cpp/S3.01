14d2c0bd3ead6d18ca94a52c9b82ac788b32e3d9
Boltz-Louis <boltzlouis44@gmail.com>
2025-12-03 16:39:58 +0100

    C'etait long :,) mais c'est fait

--- Presenter/LesEvaluations.php
+++ Presenter/LesEvaluations.php
@@ -33,11 +33,11 @@ class pageEvalProf
         return $result[0]['id'];
     }
     
-    // revoit la l'array des evaluations de la matière enseigné (pendant les ds avec comptabilisation des absences justifiées ou non)
+    // renvoit l'array des evaluations de la matière enseigné (pendant les ds avec comptabilisation des absences justifiées ou non)
     // args sert de filtre et prend les valeur inscrites en dessous
     public function lesEvaluations()
     {
-        $query = "SELECT resources.label, course_slots.course_date,course_slots.start_time, COUNT(*) as nbabs ,COUNT (CASE WHEN absences.justified = True THEN 1 END) as nb_justifications
+        $query = "SELECT resources.label, course_slots.course_date,course_slots.start_time, COUNT(*) as nbabs ,COUNT (CASE WHEN absences.justified = True THEN 1 END) as nb_justifications, course_slots.id as course_slot_id
         FROM course_slots LEFT JOIN resources ON course_slots.subject_identifier = resources.code
          LEFT JOIN absences ON course_slots.id = absences.course_slot_id
          WHERE course_slots.teacher_id = " . $this->userId . " AND course_slots.is_evaluation = True
@@ -49,6 +49,6 @@ class pageEvalProf
         
         $result = $this->db->select($query);
         return $result;
-    }
+    }       
     
 }
